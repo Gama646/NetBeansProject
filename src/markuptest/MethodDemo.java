1780170aabe7cffac29d9aa09035181135faaea7
Gama646 <sibonginkosigama646@gmail.com>
2026-05-15 10:15:21 +0200

    Java class creatd -MethodDemo

---
+++ src/markuptest/MethodDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package markuptest;
+
+/**
+ *
+ * @author sibon
+ */
+public class MethodDemo {
+    public static void main(String[] args) {
+        System.out.println("Sum from 1 to 5 is" + (1+2+3+4+5));
+    }
+    
+}
